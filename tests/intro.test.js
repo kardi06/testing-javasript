45533be90a58a933f16f7335b055d4fd19dc91a6
Kardi <kgland06@gmail.com>
2024-12-01 18:16:56 +0700

    factorial testing

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe,test, it, expect } from "vitest";
-import { fizzBuzz, max } from "../src/intro";
+import { factorial, fizzBuzz, max } from "../src/intro";
 
 describe('max', () =>{
     it('should return the fitst argument if it is greater', () => {
@@ -37,5 +37,27 @@ describe('fizzbuzz', () => {
     });
     it('should return n as a string if n is not divisible by 3 or 5', () => {
         expect(fizzBuzz(7)).toBe('7');
+    });
+});
+
+describe('factorial', () => {
+    it('should return 1 if given 0', ()=>{
+        expect(factorial(0)).toBe(1);
+    });
+    it('should return 1 if given 1', ()=>{
+        expect(factorial(1)).toBe(1);
+    });
+    it('should return 2 if given 2', ()=>{
+        expect(factorial(2)).toBe(2);
+    });
+    it('should return 6 if given 3', ()=>{
+        expect(factorial(3)).toBe(6);
+    });
+    it('should return 24 if given 4', ()=>{
+        expect(factorial(4)).toBe(24);
+    });
+
+    it('should return undefined if given negative number', ()=>{
+        expect(factorial(-1)).toBeUndefined();
     });
 });
