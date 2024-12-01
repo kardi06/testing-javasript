71c808229ba3e29e4c199b84e611eade1f0b12a5
Kardi <kgland06@gmail.com>
2024-12-01 17:55:34 +0700

    fizzbuzz testing

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe,test, it, expect } from "vitest";
-import { max } from "../src/intro";
+import { fizzBuzz, max } from "../src/intro";
 
 describe('max', () =>{
     it('should return the fitst argument if it is greater', () => {
@@ -9,7 +9,7 @@ describe('max', () =>{
         const b = 1;
         //ACT -> contohnya seperti 'Press the power button'
         const result = max(a, b);
-        //Assert -> contohnya seperti 'Verify TV is Off
+        //Assert -> contohnya seperti 'Verify TV is Off q
         expect(result).toBe(2);
 
         // untuk lebih ringkasnya , coding hanya 1 line
@@ -24,3 +24,18 @@ describe('max', () =>{
         expect(max(1, 1)).toBe(1);
     })
 })
+
+describe('fizzbuzz', () => {
+    it('should return "Fizz" if n is divisible by 3', () => {
+        expect(fizzBuzz(3)).toBe('Fizz');
+    });
+    it('should return "Buzz" if n is divisible by 5', () => {
+        expect(fizzBuzz(5)).toBe('Buzz');
+    });
+    it('should return "FizzBuzz" if n is divisible by 3 and 5', () => {
+        expect(fizzBuzz(15)).toBe('FizzBuzz');
+    });
+    it('should return n as a string if n is not divisible by 3 or 5', () => {
+        expect(fizzBuzz(7)).toBe('7');
+    });
+});
